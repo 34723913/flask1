05ed6004b956d408586e8f6d883e9dc99aaf64a8
34723913 <63636076+34723913@users.noreply.github.com>
2022-02-25 14:56:02 +0800

    Update test_flask.py
    upload功能(未完成)

--- test_flask.py
+++ test_flask.py
@@ -1,7 +1,8 @@
 from flask import Flask,render_template,request,redirect,url_for
 from flask import session
 import json
-
+import os
+import time
 app=Flask(__name__)
 @app.route('/',methods=['POST','GET'])
 def index():
@@ -47,15 +48,63 @@ def login():
 		else:
 			return render_template('login.html',alert="Your account is unregistered.")
 	return render_template('login.html')
-
-
 @app.route('/logout',methods=['GET','POST'])
 def logout ():
 	if request.method=='POST':
 		if request.values['send']=='確定':
 			session.pop('username',None)
 		return redirect(url_for('index'))
+	basepath = os.path.join(os.path.dirname(__file__), 'static', 'uploads')
+	os.mkdir(os.path.join(basepath, request.values['userid']))
 	return render_template('logout.html')
+
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload():
+	basepath = os.path.join(os.path.dirname(__file__), 'static', 'uploads')
+	dirs = os.listdir(os.path.join(basepath, session.get('username')))
+	dirs.insert(0, 'New Folder')
+	dirs.insert(0, 'Not Choose')
+
+	if request.method == 'POST':
+		flist = request.files.getlist("file[]")
+
+		for f in flist:
+			format = f.filename[f.filename.index('.'):]
+			fileName = time.time()
+			if format in ('.jpg', '.png', '.jpeg', '.HEIC', '.jfif'):
+				format = '.jpg'
+			else:
+				format = '.mp4'
+
+			if request.values['folder'] == '0':
+				return render_template('uploads.html', alert='Please choose a folder or creat a folder', dirs=dirs)
+
+			elif request.values['folder'] == '1':
+				if not os.path.isdir(os.path.join(basepath, session.get('username'), request.values['foldername'])):
+					os.mkdir(os.path.join(basepath, session.get('username'), request.values['foldername']))
+					os.mkdir(os.path.join(basepath, session.get('username'), request.values['foldername'], 'video'))
+					os.mkdir(os.path.join(basepath, session.get('username'), request.values['foldername'], 'photo'))
+
+				if format == '.mp4':
+					upload_path = os.path.join(basepath, session.get('username'), request.values['foldername'], 'video',
+											   str(fileName).replace('.', '') + str(format))
+				else:
+					upload_path = os.path.join(basepath, session.get('username'), request.values['foldername'], 'photo',
+											   str(fileName).replace('.', '') + str(format))
+
+			else:
+				if format == '.mp4':
+					upload_path = os.path.join(basepath, session.get('username'), dirs[int(request.values['folder'])],
+											   'video', str(fileName).replace('.', '') + str(format))
+				else:
+					upload_path = os.path.join(basepath, session.get('username'), dirs[int(request.values['folder'])],
+											   'photo', str(fileName).replace('.', '') + str(format))
+
+			f.save(upload_path)
+
+		return redirect(url_for('upload'))
+	return render_template('uploads.html', dirs=dirs)
 
 
 if __name__ == '__main__':
